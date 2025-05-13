e52f55c78cd91e6535cd915d564db431620a2577
andreas31603 <91518626+andreas31603@users.noreply.github.com>
2025-05-13 16:54:36 +0700

    Add lib/constants/colors.dart 1747130076484

---
+++ lib/constants/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Map<String, Color> colorMap = {
+    'Beige': Color(0xFFF5F5DC),
+    'Black': Colors.black,
+    'Blue': Colors.blue,
+    'Brown': Color(0xFF8B4513),
+    'Gray': Colors.grey,
+    'Green': Colors.green,
+    'Orange': Colors.orange,
+    'Pink': Colors.pink,
+    'Purple': Colors.purple,
+    'Red': Colors.red,
+    'White': Colors.white,
+    'Yellow': Colors.yellow,
+  };
+
+  static const List<String> colorLabels = [
+    'Beige', 'Black', 'Blue', 'Brown', 'Gray', 'Green',
+    'Orange', 'Pink', 'Purple', 'Red', 'White', 'Yellow'
+  ];
+}
